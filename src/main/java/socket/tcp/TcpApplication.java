49634c9a460e34c0b277bb4572fdd4543fa7f4e6
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-29 20:22:59 +0900

    template

---
+++ src/main/java/socket/tcp/TcpApplication.java
@@ -0,0 +1,13 @@
+package socket.tcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/socket/tcp/TcpApplicationTests.java
@@ -0,0 +1,13 @@
+package socket.tcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
