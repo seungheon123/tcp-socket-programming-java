4601dabc008a0424bc5504b7f394c6e9e231611d
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-29 20:22:35 +0900

    TCP 클라이언트

---
+++ src/main/java/socket/tcp/single/client.java
@@ -0,0 +1,32 @@
+package socket.tcp.single;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket socket = new Socket("127.0.0.1", 9999);
+            System.out.println(">echo-client is activated");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println(">");
+                String output = scanner.nextLine();
+                out.println(output);
+                out.flush();
+                if("quit".equalsIgnoreCase(output)) break;
+            }
+            socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
