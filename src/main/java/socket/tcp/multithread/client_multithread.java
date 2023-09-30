a7c1dd56fb7335a5cf75ccbd31710ab928e0d437
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-30 22:29:48 +0900

    TCP 멀티스레드

--- src/main/java/socket/tcp/multithread/client_multithread.java
+++ src/main/java/socket/tcp/multithread/client_multithread.java
@@ -1,4 +1,31 @@
 package socket.tcp.multithread;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class client_multithread {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket socket = new Socket("127.0.0.1",9999);
+            System.out.println(">echo-client is activated with port number"+socket.getLocalPort());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print(">");
+                String output = scanner.nextLine();
+                out.println(output);
+                out.flush();
+                if("quit".equalsIgnoreCase(output)) break;
+            }
+            socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/socket/tcp/multithread/server_multithread.java
+++ src/main/java/socket/tcp/multithread/server_multithread.java
@@ -1,5 +1,69 @@
 package socket.tcp.multithread;
 
-public class server_multithread {
+import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class server_multithread {
+    public static int tcpServerPort = 9999;
+    public static Scanner scanner;
+    public static void main(String[] args) throws IOException {
+        ServerSocket ssk = null;
+        Socket socket = null;
+        try {
+            ssk = new ServerSocket(tcpServerPort);
+            System.out.println("> echo-server is activated");
+            while (true) {
+                socket = ssk.accept();
+                // client가 접속할때마다 새로운 스레드 생성
+                MyThread newThread = new MyThread(socket);
+                newThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ssk!=null) {
+                try {
+                    ssk.close();
+                    System.out.println("> echo-server is de-activated");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("> echo-server error");
+                }
+            }
+        }
+    }
+}
+
+class MyThread extends Thread{
+    Socket socket;
+    public MyThread(Socket socket){
+        this.socket = socket;
+    }
+    @Override
+    public void run(){
+        try{
+            System.out.println("> client connect by IP address" + socket.getInetAddress() +
+                    "with Port number "+socket.getPort());
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while(true){
+                String input = br.readLine();
+                if("quit".equalsIgnoreCase(input)) {
+                    socket.close();
+                    break;
+                }
+                System.out.println();
+                System.out.println(">Client "+socket.getPort()+": " + input);
+            }
+        }catch (Exception e){
+            System.out.println("예외가 발생했습니ㅏㄷ");
+        }
+    }
 }
