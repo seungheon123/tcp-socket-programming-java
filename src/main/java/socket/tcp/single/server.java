43cf8b9c5da5dd0ed66e9e2a26566a8327ade847
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-30 19:52:59 +0900

    modified server code

--- src/main/java/socket/tcp/single/server.java
+++ src/main/java/socket/tcp/single/server.java
@@ -6,25 +6,38 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class server {
     public static int tcpServerPort = 9999;
     public static void main(String[] args) throws IOException {
         try {
+            //서버 생성
             ServerSocket ssk = new ServerSocket(tcpServerPort);
             System.out.println("> eco-server is activated");
-            Socket socket = ssk.accept();
-            System.out.println("> client connect by IP address" + socket.getInetAddress() + "with Port number"
-                    + socket.getPort());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            while (true) {
-                String input = in.readLine();
-                if("quit".equalsIgnoreCase(input)) break;
-                System.out.println(">Client: "+input);
+            Scanner scanner = new Scanner(System.in);
+            //Server is bind
+            while(true){
+                //Client 접속 accept
+                Socket socket = ssk.accept();
+                System.out.println(socket);
+                System.out.println("> client connect by IP address" + socket.getInetAddress() + "with Port number"
+                        + socket.getPort());
+                //Client가 보낸 데이터 출력
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while (true) {
+                    String input = in.readLine();
+                    if ("quit".equalsIgnoreCase(input)) {
+                        socket.close();
+                        break;
+                    }
+                    System.out.println(">Client: " + socket.getPort() + input);
+                }
+                System.out.print(">");
+                String message = scanner.nextLine();
+                if("quit".equalsIgnoreCase(message)) break;
             }
-            socket.close();
             ssk.close();
         }catch (IOException e){
             e.printStackTrace();
