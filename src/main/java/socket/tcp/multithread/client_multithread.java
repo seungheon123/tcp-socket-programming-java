b6544910572d7696f139b03b32c6f599bb8a35c3
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-01 19:05:50 +0900

    Server Client 멀티스레드 양방향 통신

--- src/main/java/socket/tcp/multithread/client_multithread.java
+++ src/main/java/socket/tcp/multithread/client_multithread.java
@@ -10,22 +10,35 @@ import java.util.Scanner;
 public class client_multithread {
 
     public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        Scanner scanner = new Scanner(System.in);
         try {
-            Socket socket = new Socket("127.0.0.1",9999);
+            socket = new Socket("127.0.0.1",9999);
             System.out.println(">echo-client is activated with port number"+socket.getLocalPort());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                System.out.print(">");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            while(true){
+                System.out.println(">");
                 String output = scanner.nextLine();
                 out.println(output);
                 out.flush();
                 if("quit".equalsIgnoreCase(output)) break;
+                String input = in.readLine();
+                if("quit".equalsIgnoreCase(input)) break;
+                System.out.println(">Server " +socket.getPort()+":"+ input);
             }
-            socket.close();
         }catch (IOException e){
             e.printStackTrace();
+        }finally {
+            try{
+                scanner.close();
+                socket.close();
+                System.out.println("> echo-client is de-activated");
+            }catch (IOException e){
+                System.out.println("> echo-client error");
+            }
         }
     }
 }
--- src/main/java/socket/tcp/multithread/server_multithread.java
+++ src/main/java/socket/tcp/multithread/server_multithread.java
@@ -5,15 +5,19 @@ import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class server_multithread {
     public static int tcpServerPort = 9999;
-    public static Scanner scanner;
+    public static List<Socket> clientSockets = new ArrayList<>();
+    public static PrintWriter out = null;
     public static void main(String[] args) throws IOException {
         ServerSocket ssk = null;
         Socket socket = null;
@@ -22,6 +26,7 @@ public class server_multithread {
             System.out.println("> echo-server is activated");
             while (true) {
                 socket = ssk.accept();
+                clientSockets.add(socket);
                 // client가 접속할때마다 새로운 스레드 생성
                 MyThread newThread = new MyThread(socket);
                 newThread.start();
@@ -31,6 +36,7 @@ public class server_multithread {
         } finally {
             if (ssk!=null) {
                 try {
+                    out.close();
                     ssk.close();
                     System.out.println("> echo-server is de-activated");
                 } catch (IOException e) {
@@ -40,30 +46,49 @@ public class server_multithread {
             }
         }
     }
-}
 
-class MyThread extends Thread{
-    Socket socket;
-    public MyThread(Socket socket){
-        this.socket = socket;
-    }
-    @Override
-    public void run(){
-        try{
-            System.out.println("> client connect by IP address" + socket.getInetAddress() +
-                    "with Port number "+socket.getPort());
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while(true){
-                String input = br.readLine();
-                if("quit".equalsIgnoreCase(input)) {
-                    socket.close();
-                    break;
+    static class MyThread extends Thread{
+        Socket socket;
+        public MyThread(Socket socket){
+            this.socket = socket;
+        }
+        @Override
+        public void run(){
+            BufferedReader in = null;
+            Scanner scanner = new Scanner(System.in);
+            try{
+                System.out.println("> client connect by IP address " + socket.getInetAddress() +
+                        " with Port number "+socket.getPort());
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while(true){
+                    String input = in.readLine();
+                    if("quit".equalsIgnoreCase(input)){
+                        clientSockets.remove(socket);
+                        break;
+                    }
+                    System.out.println(">Client "+socket.getPort()+": " + input);
+                    System.out.println(">");
+                    String output = scanner.nextLine();
+                    for(Socket client : clientSockets){
+                        try{
+                            System.out.println(client);
+                            out = new PrintWriter(client.getOutputStream(),true);
+                            out.println(output);
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
                 }
-                System.out.println();
-                System.out.println(">Client "+socket.getPort()+": " + input);
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                try{
+                    in.close();
+                    socket.close();
+                }catch (IOException e){
+                    System.out.println("> echo-server error");
+                }
             }
-        }catch (Exception e){
-            System.out.println("예외가 발생했습니ㅏㄷ");
         }
     }
 }
